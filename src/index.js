d7951e0be5a9b9c0a4424b2c09c5d44e55e8688d
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-06 11:15:50 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import { getUsers } from "./api/userApi";
 
 getUsers().then((result) => {
   let usersBody = "";
-
   result.forEach((user) => {
     usersBody += `<tr>
-    <td><a herf="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
     <td>${user.id}</td>
     <td>${user.firstName}</td>
-    <td>$${user.lastName}</td>
+    <td>${user.lastName}</td>
     <td>${user.email}</td>
     </tr>`;
   });
 
-  global.document.getElementById("users").innerHTML = usersBody;
+  global.document.getElementById.apply("users").innerHTML = usersBody;
 });
