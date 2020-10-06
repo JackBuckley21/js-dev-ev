9ab40f9aa8b6abc077dc3c1ff475dca2184a8aa1
Jack Buckley <jack.buckley@wrenkitchens.com>
2020-10-06 13:22:01 +0100

    More production build stuff

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,44 @@
 import path from "path";
 import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
   debug: true,
   devtool: "source-map",
   noInfo: false,
-  entry: [path.resolve(__dirname, "src/index")],
+  entry: {
+    vendor: path.resolve(__dirname, "src/vendor"),
+    main: path.resolve(__dirname, "src/index"),
+  },
   target: "web",
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "[name].js",
   },
   plugins: [
+    //Use commonsChunkPlugin to creat a separate bundle
+    //of vendor libraries so that they're cached separatley.
+    new webpack.optimize.CommonsChunksPlugin({
+      name: "vendor",
+    }),
+    // Create HTML file that includes reference to bunled JS.
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
     //Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     //minify JS
